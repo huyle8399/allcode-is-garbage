cd021b0d8cb55975326dfa0ae1f7d551b35aa8c7
huyle8399 <63323470+huyle8399@users.noreply.github.com>
2020-04-16 23:58:27 -0700

    Add Random Button

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ let css = document.querySelector('h3');
 let color1 = document.getElementById('colorpick-1');
 let color2 = document.getElementById('colorpick-2');
 let body = document.getElementById('gradient');
+let random = document.getElementById('random-color');
+
+
+function randomColor() {
+    return Math.floor(Math.random() * 16777215).toString(16);
+}
+
 
 function setGradient() {
     body.style.background = "linear-gradient(to right" + "," + color1.value + "," + color2.value + ")";
@@ -10,7 +17,14 @@ function setGradient() {
 
 
 color1.addEventListener('input', setGradient);
-
 color2.addEventListener('input', setGradient);
 
 
+random.addEventListener('click', () => {
+    color1.value = "#" + randomColor();
+    color2.value = "#" + randomColor();
+    
+    body.style.background = "linear-gradient(to right" + "," + color1.value + "," + color2.value + ")";
+
+    css.textContent = "linear-gradient(to right" + "," + color1.value + "," + color2.value + ")";
+});
